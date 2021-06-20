05101178ceb40f38c3f713a5ae9d276ad0fbf6c2
Pilseong Heo <heops79@gmail.com>
2021-06-20 13:48:40 +0900

    add array

---
+++ pkg/array/Array.go
@@ -0,0 +1,37 @@
+package array
+
+import "fmt"
+
+type Array struct {
+	arr []int
+}
+
+func (a *Array) Add(v int) {
+	a.arr = append(a.arr, v)
+}
+
+func (a *Array) Display() {
+	fmt.Printf("content start\n")
+	for _, v := range a.arr {
+		fmt.Printf("%v ", v)
+	}
+	fmt.Println("\ncontent end")
+}
+
+func (a *Array) Insert(i, v int) {
+
+	// if the index is the same as length of the array then add the new value to the end of the array
+	// it would be add value to the empty array
+	if len(a.arr) == i {
+		a.arr = append(a.arr, v)
+		return
+	}
+	a.arr = append(a.arr[:i+1], a.arr[i:]...)
+	a.arr[i] = v
+}
+
+func (a *Array) Delete(i int) {
+	if i >= 0 && i < len(a.arr) {
+		a.arr = append(a.arr[:i], a.arr[i+1:]...)
+	}
+}
---
+++ pkg/array/Array_test.go
@@ -0,0 +1,64 @@
+package array
+
+import (
+	"log"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+
+	var arr Array
+
+	arr.Add(1)
+	arr.Add(2)
+	arr.Add(3)
+
+	if len(arr.arr) != 3 {
+		t.Errorf("Expected 3 but got %v", len(arr.arr))
+	}
+	log.Println(arr.arr)
+}
+
+func TestDisplay(t *testing.T) {
+	var arr Array
+
+	arr.Add(1)
+	arr.Add(2)
+	arr.Add(3)
+
+	arr.Display()
+}
+
+func TestInsert(t *testing.T) {
+
+	var arr Array
+
+	arr.Add(1)
+	arr.Add(3)
+	arr.Add(4)
+
+	arr.Display()
+
+	arr.Insert(1, 2)
+
+	if len(arr.arr) != 4 || arr.arr[1] != 2 {
+		t.Errorf("Expected size 4 and the value 2 but got size of %v, value %v", len(arr.arr), arr.arr[1])
+	}
+
+	arr.Display()
+}
+
+func TestDelete(t *testing.T) {
+	var arr Array
+
+	arr.Add(1)
+	arr.Add(2)
+	arr.Add(3)
+
+	arr.Delete(1)
+
+	if len(arr.arr) != 2 {
+		t.Errorf("Expected 2 but got %v", len(arr.arr))
+	}
+	arr.Display()
+}
