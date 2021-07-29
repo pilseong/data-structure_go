88c45298cfa7879faaee693dfd59ea386a46f88c
Pilseong Heo <heops79@gmail.com>
2021-07-29 23:47:51 +0900

    merge sort done

--- pkg/sorting/merge.go
+++ pkg/sorting/merge.go
@@ -4,7 +4,7 @@ import "log"
 
 // Merge two different sections inside an array
 // one is from [l, mid), another is from [mid, h)
-func Merge(arr []int, l, mid, h int) {	
+func Merge(arr []int, l, mid, h int) {
 	var i, j, k int
 	var res []int
 
@@ -20,7 +20,6 @@ func Merge(arr []int, l, mid, h int) {
 			i++
 		}
 	}
-
 
 	// slice is totally different from array
 	if i < mid {
@@ -42,4 +41,30 @@ func Merge(arr []int, l, mid, h int) {
 	}
 
 	// log.Println((arr))
+}
+
+func MergeSort(arr []int) {
+	var j, p int
+
+	for p = 2; p <= len(arr); p = p * 2 {
+
+		for j = 0; j+p <= len(arr); j = j + p {
+			log.Print(j, (j+j+p)/2, j+p, p)
+			Merge(arr, j, (j+j+p)/2, j+p)
+		}
+
+		// if the size of array is not divided by power of 2
+		// get the last slice, merge them
+		if len(arr)%p != 0 {
+
+			log.Print("l ", j, j+(len(arr)-j-(p/2)), len(arr), p)
+			Merge(arr, j, j+(len(arr)-j-(p/2)), len(arr))
+		}
+	}
+
+	// last merge for the remaining elements
+	if p/2 < len(arr) {
+		log.Print("e ", 0, p/2, len(arr), p)
+		Merge(arr, 0, p/2, len(arr))
+	}
 }
--- pkg/sorting/merge_test.go
+++ pkg/sorting/merge_test.go
@@ -6,26 +6,9 @@ import (
 )
 
 func TestMergeSort(t *testing.T) {
-	arr := []int{3, 2, 1, 7, 30, 5, 24, 18, 9}
+	arr := []int{3, 2, 1, 7, 30, 5, 24} //, 18, 9, 11}
 
-	var j, p int
-	
-
-	for p = 2; (p-1)*2 <= len(arr); p = p * 2 {
-		for j = 0; j+p <= len(arr); j = j + p {
-			log.Print(j, (j+j+p)/2, j+p, p)
-			Merge(arr, j, (j+j+p)/2, j+p)
-		}
-
-		if len(arr) % p != 0 {
-			log.Print("etc ", j, (j+j+p)/2, j+p, p)
-			Merge(arr, j, (j+j+p)/2, len(arr))
-		}
-	}
-
-	if p/2 < len(arr) {
-		Merge(arr, 0, p/2, len(arr))
-	}
+	MergeSort(arr)
 
 	for _, d := range arr {
 		log.Println(d)
