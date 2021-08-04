fc8e0b4609c4638a12a8706d5f976e588135efc3
Philip Heur <heops79@gmai.com>
2021-08-05 00:13:07 +0900

    package updated

---
+++ pkg/arrays/Array.go
@@ -0,0 +1,37 @@
+package arrays
+
+import "fmt"
+
+// Array is using slice for base data-structure
+type Array struct {
+	arr []int
+}
+
+func (a *Array) Add(v int) {
+	a.arr = append(a.arr, v)
+}
+
+func (a *Array) Display() {
+	for _, v := range a.arr {
+		fmt.Printf("%v ", v)
+	}
+	fmt.Println()
+}
+
+func (a *Array) Insert(i, v int) {
+
+	// if the index is the same as length of the array then add the new value to the end of the array
+	// it would be add value to the empty array
+	if len(a.arr) == i {
+		a.arr = append(a.arr, v)
+		return
+	}
+	a.arr = append(a.arr[:i+1], a.arr[i:]...)
+	a.arr[i] = v
+}
+
+func (a *Array) Delete(i int) {
+	if i >= 0 && i < len(a.arr) {
+		a.arr = append(a.arr[:i], a.arr[i+1:]...)
+	}
+}
--- pkg/sorting/quck_test.go
+++ pkg/sorting/quck_test.go
@@ -1,13 +1,16 @@
 package sorting
 
 import (
+	"log"
+	"math"
 	"testing"
 )
 
 func TestQuickSort(t *testing.T) {
 
-	arr := []int{20, 3, 32, 99, 53, 2, 5, 6, 8, 23, 21, 235, 10, 45, 33, int(^uint(0) >> 1)}
+	arr := []int{20, 3, 32, 99, 53, 2, 5, 6, 8, 23, 21, 235, 10, 45, 33, math.MaxInt64}
 
 	QuickSort(arr, 0, len(arr)-1)
 
+	log.Println(arr)
 }
